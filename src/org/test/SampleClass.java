7dfcdd04a36fd9bc148f9c631e3761759fc8cabd
gittocken <thilagachandru02@gmail.com>
2022-09-02 21:04:49 +0530

    firstgit

---
+++ src/org/test/SampleClass.java
@@ -0,0 +1,20 @@
+package org.test;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		
+		int num,a,b;
+		num = 100;
+		System.out.println("this is num:"+num);
+		a=num*2;
+		System.out.println("The value of num *2 is");
+		System.out.println(a);
+		b=num*5;
+		System.out.println("the value of num *5 is");
+		System.out.println(b);
+		
+		
+	}
+	
+}
